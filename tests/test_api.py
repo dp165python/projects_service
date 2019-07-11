e6e5b22c6e2532a53d304b8fa72475144e5a6bdc
pc <eugenezaharchenko@gmail.com>
2019-07-11 08:36:53 +0300

    tests. Delete fixed

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,46 @@
+import os
 import unittest
-
 import json
+
+from flask_migrate import Migrate
+
 from core.app import app
+from core.config import db
+
+migrate = Migrate()
+
 # python -m unittest test_api to run tests
+
+# TEST_DB = 'test_api.db'
 
 
 class TestMyAppCRUD(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
+
+    # app.config['TESTING'] = True
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+    # self.app = app.test_client()
+    # db.drop_all()
+    # db.create_all()
+
+    # executed prior to each test
+    # def setUp(self):
+    #     app.config['TESTING'] = True
+    #     app.config['WTF_CSRF_ENABLED'] = False
+    #     app.config['DEBUG'] = False
+    #     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://eugene:1401@localhost/test_api'
+    #
+    #     # db.create_all()
+    #     with app.app_context():
+    #         db.init_app(app)
+    #         migrate.init_app(app, db)
+    #     self.app = app.test_client()
+
+
+    # executed after each test
+    def tearDown(self):
+        pass
 
     def test_getting_all_projects(self):
         # HTTP GET request to the application on certain path
@@ -15,59 +48,57 @@ class TestMyAppCRUD(unittest.TestCase):
 
         # assert the status code and type of response
         self.assertEqual(prj.status_code, 200)
-        self.assertNotEqual(prj.status_code, 400)
         self.assertEqual(prj.content_type, 'application/json')
-
-        # assert the response data
         # self.assertEqual(prj.data, b'{"message": "There are no projects"}\n')
 
     # Test data:
-    data = {
-        'status': 'create_schema',
-        'name': 'eugene',
-        'contract_id': '18f441a0-d0b1-4b22-b16b-c1e718dab64d'
-    }
+    # data = {
+    #     'status': 'create_schema',
+    #     'name': 'eugene',
+    #     'contract_id': '18f441a0-d0b1-4b22-b16b-c1e718dab64d'
+    # }
 
-    # def test_posting_project(self):
-    #     # HTTP POST request
-    #     prj = self.app.post('/projects', data=json.dumps(self.data), content_type='application/json')
+    def test_posting_project(self):
+        # HTTP POST request
+        prj = self.app.post('/projects', data=None)
+        # data = json.dumps(self.data), content_type = 'application/json'
+        self.assertEqual(prj.content_type, 'application/json')
+        # self.assertEqual(prj.status_code, 201)
+
+    # def test_initialize_projects_post(self):
+    #     # HTTP POST request with no data
+    #     prj = self.app.post('/projects', data=None)
     #     self.assertEqual(prj.status_code, 201)
 
-    def test_posting_projects_denial(self):
-        # HTTP POST request with no data
-        prj = self.app.post('/projects', data=None)
-        self.assertEqual(prj.status_code, 400)
-
-    # def test_getting_certain_project(self, _id='862d8628-29c6-48c0-9303-8bfec1c98d56'):
-    #     # HTTP GET request to the application on certain path
-    #     prj = self.app.get('/projects/{}'.format(_id))
-    #
-    #     # assert the status code
-    #     self.assertEqual(prj.status_code, 200)
+    def test_getting_certain_project(self, _id='e3cb6cec-2945-4490-828d-f8267d925ca3'):
+        # HTTP GET request to the application on certain path
+        prj = self.app.get('/projects/{}'.format(_id))
+        self.assertEqual(prj.status_code, 200)
 
     def test_getting_certain_project_denial(self, _id='00000000-0000-0000-0000-000000000000'):
         # HTTP GET request with incorrect id
         prj = self.app.get('/projects/{}'.format(_id))
         self.assertEqual(prj.status_code, 404)
 
-    # def test_putting_certain_project(self, _id='862d8628-29c6-48c0-9303-8bfec1c98d56'):
-    #     # HTTP PUT request to the application on certain path
-    #     prj = self.app.put('/projects/{}'.format(_id))
-    #
-    #     # assert the status code
+    # def test_patching_certain_project(self, _id='e3cb6cec-2945-4490-828d-f8267d925ca3'):
+    #     # HTTP PATCH request to the application on certain path
+    #     prj = self.app.patch('/projects/{}'.format(_id))
     #     self.assertEqual(prj.status_code, 200)
 
-    def test_putting_certain_project_denial(self, _id='00000000-0000-0000-0000-000000000000'):
-        # HTTP PUT request with incorrect id
-        prj = self.app.put('/projects/{}'.format(_id))
-        self.assertEqual(prj.status_code, 400)
+    # def test_patching_certain_project_denial(self, _id='00000000-0000-0000-0000-000000000000'):
+    #     # HTTP PATCH request with incorrect id
+    #     prj = self.app.patch('/projects/{}'.format(_id))
+    #     self.assertEqual(prj.status_code, 500)
 
-    #     # assert the status code and type of response
+    # def test_delete_certain_project(self, _id='e3cb6cec-2945-4490-828d-f8267d925ca3'):
+    #     # HTTP DELETE request to the application on certain path
+    #     prj = self.app.delete('/projects/{}'.format(_id))
     #     self.assertEqual(prj.status_code, 200)
-    #     self.assertNotEqual(prj.status_code, 400)
-    #
-    #     # assert the response data
-    #     self.assertEqual(prj.data, b'{"message": "There are no projects"}\n')
+
+    def test_deleting_certain_project_denial(self, _id='00000000-0000-0000-0000-000000000000'):
+        # HTTP GET request with incorrect id
+        prj = self.app.delete('/projects/{}'.format(_id))
+        self.assertEqual(prj.status_code, 404)
 
     # def test_add(self):
     #     rv = self.app.get('/add/2/3')
