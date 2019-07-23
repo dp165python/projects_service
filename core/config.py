f8156211e8675655a2e9ff9a371bf5b77d103948
Arthur543 <arthur54342@gmail.com>
2019-07-23 19:45:49 +0300

    add manage.py

--- core/config.py
+++ core/config.py
@@ -4,16 +4,16 @@ from core.constants import APP_ENV_DEV, APP_ENV_PROD, APP_ENV_TEST
 
 
 class Config:
-    DB_USER = os.environ.get('DB_USER', 'eugene')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', '1401')
+    DB_USER = os.environ.get('DB_USER', 'postgres')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
     DEFAULT_DB = os.environ.get('DEFAULT_DB', 'postgres')
     DB_HOST = os.environ.get('DB_HOST', 'localhost')
     DB_PORT = os.environ.get('DB_PORT', 5432)
     DEBUG = False
     HOST = '127.0.0.1'
     TOKEN = os.environ.get('TOKEN', None)
-    DB_NAME = os.environ.get('DB_NAME', 'projects')
-    DB_URI = 'postgresql://{user}:{password}@{host}:{port}/{db_name}'
+    DB_NAME = os.environ.get('DB_NAME', 'postgres')
+    DB_URI = 'postgresql://{user}@{host}:{port}/{db_name}'
 
 
 class DevelopmentConfig(Config):
